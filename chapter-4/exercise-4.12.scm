148fc709c9e1fd69782c1479733311c6cdcab124
Geoff Lanotte <geofflanotte@gmail.com>
2017-08-30 17:15:44 -0500

    through section 4.1.3

---
+++ chapter-4/exercise-4.12.scm
@@ -0,0 +1,31 @@
+(define (search-env env found-proc not-found-proc)
+  (define (scan vars vals)
+    (cond ((null? vars) (not-found-proc env))
+          ((eq? var (car vars)) (found-proc (vars vals)))
+          (else (scan (cdr vars) (cdr vals)))))
+  (if (eq? env the-empty-environment)
+    (error "Unbound variable" var)
+    (let ((frame (first-frame env)))
+      (scan (frame-variables frame)
+            (frame-values frame)))))
+
+(define (deep-search-env env found-proc)
+  (search-env env
+              found-proc
+              (lambda (env)
+                (search-env (enclosing-environment env)))))
+
+(define (lookup-variable-value var env)
+  (deep-search-env env
+                   (lambda (vars vals) (car vals))))
+
+(define (set-variable-value! var val env)
+  (deep-search-env env
+                   (lambda (env) (set-car! vals val))))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (search-env env
+                  (lambda (var vals) (set-car! vals val))
+                  (lambda (env) (add-binding-to-frame! var val frame))))))
